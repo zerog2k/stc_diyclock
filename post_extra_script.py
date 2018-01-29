b570b23752bb47382bdba1b5a8dc44791237034e
Jens Jensen <af5mi@yahoo.com>
2018-01-29 07:27:07 -0600

    update gps branch for platformio support

---
+++ post_extra_script.py
@@ -0,0 +1,21 @@
+''' custom script for platformio '''
+
+from os.path import join
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+
+#print "post_extra_script running..."
+#print env.Dump()
+
+# compiler and linker flags dont work very well in build_flags of platformio.ini - need to set them here
+env.Append(
+    CFLAGS = [
+    "--disable-warning", 126,
+    "--disable-warning", 59
+    ],
+    LINKFLAGS = [
+        "--data-loc", 0x30
+    ]
+)
+
