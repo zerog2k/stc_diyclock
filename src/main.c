06b1c25ccfb4b182a15c6d939ef76a3d7bee2d71
venus@trg.ru <venus@rontel.ru>
2020-03-22 12:19:50 +0500

    reset correction timer on seconds zeroing

--- src/main.c
+++ src/main.c
@@ -718,8 +718,12 @@ int main()
 #else
                     kmode = K_NORMAL;
 #endif
-                } else if (ev == EV_S2_SHORT)
+                } else if (ev == EV_S2_SHORT) {
                     ds_sec_zero();
+#if defined(WITH_MONTHLY_CORR) && WITH_MONTHLY_CORR != 0
+                    corr_remaining = RUNTIME_PER_SEC;
+#endif
+                }
 #ifdef DEBUG
                 else if (ev == EV_S1S2_LONG)
                     kmode = K_DEBUG;
