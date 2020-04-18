c4c3adbd2c172424b2c300e09b1628dc7334822d
venus@trg.ru <venus@rontel.ru>
2020-04-19 00:54:58 +0500

    serial sync from NMEA devices (GPS, ES8266/nodemcu with NTP)

---
+++ src/nmea.h
@@ -0,0 +1,166 @@
+#include <time.h>
+
+#define LOC_TZ_SEC 18000 // TZ offset in seconds (Urals+5 = 5*3600 = 18000)
+#define LOC_DST 0 // daylight saving time 0/1 == off/on
+
+#define BAUDRATE 9600 // serial port speed (9600 - standard for GPS)
+
+#define MIN_NMEA_PAUSE 14400 // min pause between syncs (sec) - 4hr
+
+volatile uint32_t sync_remaining = 0;
+
+void uart1_init()
+{
+    P_SW1 |= (1 << 6);          // move UART1 pins -> P3_6:rxd, P3_7:txd
+    // UART1 use Timer2
+    T2L = (65536 - (FOSC / 4 / BAUDRATE)) & 0xFF;
+    T2H = (65536 - (FOSC / 4 / BAUDRATE)) >> 8;
+    SM1 = 1;                    // serial mode 1: 8-bit async
+    AUXR |= 0x14;               // T2R: run T2, T2x12: T2 clk src sysclk/1
+    AUXR |= 0x01;               // S1ST2: T2 is baudrate generator
+    ES = 1;                     // enable uart1 interrupt
+    EA = 1;                     // enable interrupts
+    REN = 1;
+}
+
+#define NMEA_LINE_LEN_MAX 84
+__pdata char ubuf[NMEA_LINE_LEN_MAX];
+volatile int8_t uidx = 0;
+__code const char NMEA_TOKEN[] = "$GPRMC";
+
+volatile enum {
+    NMEA_NONE = 0,
+    NMEA_START,
+    NMEA_PARSE,
+    NMEA_SET
+} nmea_state = NMEA_NONE;
+
+// return pointer to comma N num in ubuf
+char *nmea_comma(uint8_t num)
+{
+    int8_t i;
+    for (i = 0; i < uidx; i++)
+        if (ubuf[i] == ',' && !--num)
+            return ubuf + i;
+    return NULL;
+}
+
+// just simple memcmp, ret 0 == equal
+int8_t nmea_cmp(char *dst, char *src, uint8_t n)
+{
+    while (n--)
+        if (*dst++ != *src++)
+            return 1;
+    return 0;
+}
+
+// just simple memcpy
+void nmea_cpy(uint8_t *dst, uint8_t *src, uint8_t n)
+{
+    while (n--)
+        *dst++ = *src++;
+}
+
+// ret 0 == valid crc
+uint8_t nmea_crc_check()
+{
+    char *p;
+    uint8_t crc;
+    for (crc = 0, p = ubuf + 1; *p != '*'; p++)
+        crc ^= *p;
+    return ((((*(++p) - '0') & 0x0f) << 4) & ((*(++p) - '0') & 0x0f)) ^ crc;
+}
+
+void uart1_isr() __interrupt 4 __using 2
+{
+    char ch;
+    char *p;
+    if (RI) {
+        RI = 0;                 // clear int
+        ch = SBUF;
+        switch (ch) {
+        case '$':
+            uidx = 0;
+            nmea_state = NMEA_START;
+            break;
+        case '\n':
+            if (nmea_state != NMEA_START)
+                nmea_state = NMEA_NONE;
+            else
+                nmea_state = NMEA_PARSE;
+            break;
+        default:
+            if (nmea_state != NMEA_START)
+                nmea_state = NMEA_NONE;
+            break;
+        }
+        if (nmea_state != NMEA_NONE) {
+            ubuf[uidx++] = ch;
+            if (nmea_state == NMEA_PARSE) {
+                // $GPRMC,232231.00,A,,,,,,,170420,,,*27
+                if (uidx >= sizeof(NMEA_TOKEN) - 1 + 12 + 1 + 5 + 12 && // 12 commas, A , *XX\r\n, hhmmss DDMMYY
+                    !nmea_cmp(ubuf, NMEA_TOKEN, sizeof(NMEA_TOKEN) - 1) && // correct head
+                    (p = nmea_comma(2)) && *(p + 1) == 'A' && // valid NMEA
+                    nmea_comma(2) - nmea_comma(1) >= 7 && // time length
+                    nmea_comma(10) - nmea_comma(9) == 7 && // date length
+                    (p = nmea_comma(12)) && (*(p + 1) == '*' || *(++p + 1) == '*') &&
+                    p + 6 - ubuf == uidx && // correct length
+                    *(p + 4) == '\r' && *(p + 5) == '\n' && // correct tail
+                    nmea_crc_check()) { // correct crc
+                    REN = 0;
+                    nmea_state = NMEA_SET;
+                    loop_gate = 1;
+                }
+            } else if (uidx >= NMEA_LINE_LEN_MAX) {
+                uidx = 0;
+                nmea_state = NMEA_NONE;
+            }
+        }
+    }
+}
+
+uint8_t char2int(char *p)
+{
+    return (*p - '0') * 10 + (*(p + 1) - '0');
+}
+
+// method by Tomohiko Sakamoto, 1992, accurate for any Gregorian date
+// returns 0 = Sunday, 1 = Monday, etc.
+
+uint8_t dayofweek(uint16_t y, uint8_t m, uint8_t d)
+{   // 1 <= m <= 12,  y > 1752 (in the U.K.)
+    static uint8_t t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+    y -= m < 3;
+    return (y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7;
+}
+
+void nmea2localtime(void)
+{
+    char *p;
+    struct tm t;
+    // $GPRMC,232231.00,A,,,,,,,170420,,,*27
+    p = nmea_comma(9) + 1;
+    t.tm_mday = char2int(p);          // day
+    t.tm_mon = char2int(p+2) - 1;     // month, 0 = jan
+    t.tm_year = char2int(p+4) + 100;  // year - 1900
+    p = nmea_comma(1) + 1;
+    t.tm_hour = char2int(p);
+    t.tm_min = char2int(p+2);
+    t.tm_sec = char2int(p+4);
+    t.tm_isdst = LOC_DST;
+    //time_t ts;
+    //ts = mktime(&t);  // make unix timestamp
+    //ts += LOC_TZ_SEC + 1; // +1 to compensate transfer/delays
+    //nmea_cpy((char *) &t, (char *) localtime(&ts), sizeof(struct tm));
+    //ts = mktime(&t); // just update struct tm with weekday set
+    //ds_writebyte(DS_ADDR_WEEKDAY, t.tm_wday + 1);
+    ds_writebyte(DS_ADDR_DAY, ds_int2bcd(t.tm_mday));
+    ds_writebyte(DS_ADDR_MONTH, ds_int2bcd(t.tm_mon + 1));
+    ds_writebyte(DS_ADDR_YEAR, ds_int2bcd(t.tm_year - 100));
+    ds_writebyte(DS_ADDR_WEEKDAY, dayofweek(t.tm_year+1900, t.tm_mon+1, t.tm_mday) + 1);
+    ds_writebyte(DS_ADDR_HOUR,    ds_int2bcd(t.tm_hour));
+    ds_writebyte(DS_ADDR_MINUTES, ds_int2bcd(t.tm_min));
+    ds_writebyte(DS_ADDR_SECONDS, 0b10000000); // set CH, stop clock
+    ds_writebyte(DS_ADDR_SECONDS, ds_int2bcd(t.tm_sec)); // clear CH, start clock
+
+}
