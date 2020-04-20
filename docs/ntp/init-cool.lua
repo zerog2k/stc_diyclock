95cbada18a026badaa315373aec139ea43f229d6
venus@trg.ru <venus@rontel.ru>
2020-04-20 10:11:59 +0500

    included ESP8266 init script with deep sleep support (no heating)

--- docs/ntp/init-cool.lua
+++ docs/ntp/init-cool.lua
@@ -1,24 +1,24 @@
 uart.setup(1, 9600, 8, uart.PARITY_NONE, uart.STOPBITS_1, 1)
 uart.write(1, "nodemcu loaded\n")
 
-station_cfg={}
-station_cfg.ssid="nautilus" -- SSID of your WiFi network
-station_cfg.pwd="xxxxxxxxxx"  -- password of your WiFi network
-station_cfg.auto=true
-station_cfg.save=true
--- set your static network parameters here
+station_cfg = {}
+station_cfg.ssid = "RON_G" -- SSID of your WiFi network
+station_cfg.pwd = "xxxxxxxxxxxxxx"  -- password of your WiFi network
+station_cfg.auto = false
+station_cfg.save = true
+
+DHCP = 1
+-- set your static network parameters here if DHCP == 0
 net_ip     = "10.10.10.110"
 net_mask   = "255.255.255.0"
 net_gate   = "10.10.10.1"
 ntp_server = "pool.ntp.org"
 
-timestring = 0
 uarttimestring = 0
 rtcGood = 0
 -- UTCTZ = 5                   -- your timezone (unix TZ) offset
-sync_delay = 480            -- sync once per X mins (480 == 8hrs)
-sync_att = sync_delay
-                        
+sync_att = 0
+
 local function check_int(n)
     -- checking not float
     if (n - math.floor(n) > 0) then
@@ -96,21 +96,21 @@ local function to_hex(n)
     if (type(n) ~= "number") then
         error("non-number type passed in.")
     end
-   
+
     -- checking not float
     if (n - math.floor(n) > 0) then
         error("trying to apply bitwise operation on non-integer!")
     end
-   
+
     if (n < 0) then
         -- negative
         n = bit.tobits(bit.bnot(math.abs(n)) + 1)
         n = bit.tonumb(n)
     end
-   
+
     hex_tbl = {'A', 'B', 'C', 'D', 'E', 'F'}
     hex_str = ""
-   
+
     while (n ~= 0) do
         last = n % 16
         if (last < 10) then
@@ -137,14 +137,13 @@ function UpdateRtc()
     end
     -- tm = rtctime.epoch2cal((rtctime.get()+1)+UTCTZ*3600)
     tm = rtctime.epoch2cal(rtctime.get())
-    timestring = string.format("%d %04d-%02d-%02d %02d:%02d:%02d", tm["wday"], tm["year"], tm["mon"], tm["day"], tm["hour"], tm["min"], tm["sec"])
     if (tm["year"] >= 2000) then
         tm["year"] = tm["year"] - 2000;
     else
         tm["year"] = 0;
     end
     uarttimestring = string.format("$GPRMC,%02d%02d%02d.00,A,,,,,,,%02d%02d%02d,,,*",
-     			   tm["hour"], tm["min"], tm["sec"], tm["day"], tm["mon"], tm["year"]);    
+                           tm["hour"], tm["min"], tm["sec"], tm["day"], tm["mon"], tm["year"]);    
     local crc = 0
     for i=2,string.len(uarttimestring)-1 do
         crc = bit_xor(crc, string.byte(uarttimestring,i));
@@ -158,79 +157,27 @@ function UpdateRtc()
 end
 
 function PrintUart()
+    print("ESP8266 wifi : " .. wifi.sta.getip())
     UpdateRtc()
     if (rtcGood == 1) then
-        if (sync_att == sync_delay) then
-            uart.write(1, uarttimestring)
-            print("uart sync sent")
-            sync_att = 0
-        end
+        uart.write(1, uarttimestring)
+        print("uart sync sent")
         sync_att = sync_att + 1
+        if (sync_att >= 2) then
+            print("going to deep sleep mode")
+            for i=1,1000000 do end
+            rtctime.dsleep(1800000000); -- 1/2 hrs
+        end
     end
 end
 
 wifi.setmode(wifi.STATION)
 wifi.sta.config(station_cfg)
+if DHCP == 0 then
+    wifi.sta.setip({ip=net_ip,netmask=net_mask,gateway=net_gate})
+end
 
-wifi.sta.setip({ip=net_ip,netmask=net_mask,gateway=net_gate})
-
-print("ESP8266 mode is: " .. wifi.getmode())
-print(wifi.sta.getip())
  
-if (srv) then
-    srv:close()
-    srv=nil
-end
-
-srv=net.createServer(net.TCP, 4)
-print("Server created on " .. wifi.sta.getip())
-
-function receiver(sck, request)
-    local response = {}
-    -- if you're sending back HTML over HTTP you'll want something like this instead
-    -- local response = {"HTTP/1.0 200 OK\r\nServer: NodeMCU on ESP8266\r\nContent-Type: text/html\r\n\r\n"}
-    response[#response + 1] = "HTTP/1.1 200 OK\r\n"
-    response[#response + 1] = "Content-type: text/html\r\n"
-    response[#response + 1] = "Connection: close\r\n\r\n"
-    response[#response + 1] = "<html><head>"
-    response[#response + 1] = '<title>Clock</title>'
-    response[#response + 1] = '<meta http-equiv="refresh" content="5">'
-    response[#response + 1] = "<style>"
-    response[#response + 1] = 'body {width:100% font-size:52px}'
-    response[#response + 1] = 'h1 {font-size:32px}'
-    response[#response + 1] = 'button {font-size:42pxheight:4emwidth:30%}'
-    response[#response + 1] = "</style>"
-    response[#response + 1] = "</head><body>"
-    response[#response + 1] = "<h1>Clock</h1>"
-    response[#response + 1] = '<div class="container">'
-    response[#response + 1] = "<p>Time RTC: ".. timestring .." </p>"
-    response[#response + 1] = "<p>NMEA: ".. uarttimestring .." </p>"
-    response[#response + 1] = "<p>sync: ".. rtcGood .." </p>"
-
-    response[#response + 1] = '</div>'
-    response[#response + 1] = "</body></html>"
-
-    -- sends and removes the first element from the 'response' table
-    local function send(localSocket)
-        if (#response > 0) then
-            localSocket:send(table.remove(response, 1))
-        else
-            localSocket:close()
-            response = nil
-        end
-    end
-
-    -- triggers the send() function again once the first chunk of data was sent
-    sck:on("sent", send)
-
-    send(sck)
-end
-
-srv:listen(80, function(conn)
-  conn:on("receive", receiver)
-end)
-
-
 function ntpSyncGood(sec, usec, server, info)
     print('ntp synced', sec, usec, server)
     offset_s = info.offset_s or 0
@@ -251,12 +198,17 @@ function ntpSyncFail()
     rtcGood = 0
 end
 
-print("starting sync")
+
+function wifi_connected()
+    print("starting sync")
+    sntp.sync(ntp_server, ntpSyncGood, ntpSyncFail, 1 )
+end
+
+wifi.sta.connect(wifi_connected)
 
 -- sync time / repeat after 1000 sec
-sntp.sync(ntp_server, ntpSyncGood, ntpSyncFail, 1 )
 
 uartTimer = tmr.create()
 uartTimer:register(10000, tmr.ALARM_AUTO, PrintUart)
-uartTimer:interval(60000) -- Maximum value is 6870947 (1:54:30.947).
+uartTimer:interval(10000) -- Maximum value is 6870947 (1:54:30.947).
 uartTimer:start()
